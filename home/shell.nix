2bac67f38ea19c7ea3b370851ebb9e7ba2f2c95e
iamkaran <kforkaranveer@gmail.com>
2026-04-14 15:45:24 +0530

    shell.nix - Add env for platform (Davinci Resolve)

--- home/shell.nix
+++ home/shell.nix
@@ -17,6 +17,7 @@
       export DEV='/etc/nixos/modules/dev.nix'
       export HM='/etc/nixos/home.nix'
       export NIX='/etc/nixos'
+      export QT_QPA_PLATFORM=xcb
 
       # ALIASES
       alias bat='upower -i $(upower -e | grep battery) | grep -E "state|time to|percentage"'
